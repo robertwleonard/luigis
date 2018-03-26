49cbb378f2ac6af460823cd912876ce0a38e9949
Robert W. Leonard <robert@draknor.com>
2018-03-26 16:47:20 -0500

    Start admin features and shipped orders

---
+++ luigis/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using luigis.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace luigis.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
